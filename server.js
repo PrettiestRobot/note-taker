d12b0106c3c181ce532fc8b34f685e22e3edf84d
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-06-15 23:51:07 -0400

    routes working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const path = require('path');
-const dbData = require('./db/db.json');
+const express = require("express");
+const path = require("path");
+const dbData = require("./db/db.json");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,38 +10,59 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-    
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 app.get("/api/notes", (req, res) => res.json(dbData));
 
 app.post("/api/notes", (req, res) => {
+  //Destructure req.body
+  const { title, text } = req.body;
+  // If title and text are present...
+  if (title && text) {
+    //create a variable containing newNote object
+    const newNote = {
+      id,
+      title,
+      text,
+    };
+    // Retrieve the contents of db.json
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json("Error in reading db.json");
+      }
+      // Save the contents of db.json as a variable and push the newNot object into the array
+      let notes = JSON.parse(data);
+      notes.push(newNote);
 
-    //Destructure req.body
-    const { title, text } = req.body;
-    // If title and text are present...
-    if (title && text) {
-        //create a variable containing newNote object
-        const newNote = {
-            title,
-            text
-        };
-        //Convert newNote object to string
-        const noteString = JSON.stringify(newNote);
+      // Stringify and return updated array to db.json
+      fs.writeFile("./db/db.json", JSON.stringify(notes), (err) =>
+        err
+          ? console.error(err)
+          : console.log(`Note for ${newNote.title} has been added!`)
+      );
 
-        console.log(noteString);
-    }
-
+      const response = {
+        status: "success",
+        body: newNote,
+      };
+      console.log(response);
+      res.status(201).json(response);
+    });
+  } else {
+    res.status(500).json("Error in posting note");
+  }
 });
 
+
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
+  console.log(`listening on port ${PORT}`);
 });
